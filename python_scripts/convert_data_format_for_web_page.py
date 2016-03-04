647b37f26b68a3f3af85cf35fe86402d73d16fa4
Mohammed Zia <mohammed.zia33@gmail.com>
2016-03-04 17:38:20 +0200

    Added script to convert OSM and filtered activity points into desired format for our web-page

---
+++ python_scripts/convert_data_format_for_web_page.py
@@ -0,0 +1,77 @@
+import os
+import csv
+
+
+# This list will hold final selected activity points data
+data_filtered_checked_osm = list()
+
+
+# Open act_pt_checked_against_osm.csv file and collect contained data.
+proj_dir = os.getcwd()
+rel_file_path = "data/act_pt_checked_against_osm.csv";
+abs_file_path = os.path.join(proj_dir, rel_file_path)
+with open(abs_file_path, 'rb') as new_data_csv:
+	rows = csv.reader(new_data_csv, delimiter = ',')
+	for row in rows:
+		# Append rows into data_filtered_checked_osm list
+		data_filtered_checked_osm.append(row);
+
+
+# Create text file containing Bus Stops derived from activity_points.geojson file in suitable format for our web-page 
+rel_file_path = "data/activity_pts_filtered_for_webpage.txt";
+abs_file_path = os.path.join(proj_dir, rel_file_path)
+with open(abs_file_path, 'w') as f:
+	# Making text file for bus stops data.
+	string_for_act_pt = "";
+	for feature in data_filtered_checked_osm:
+		if (feature[0] == "x" and feature[1] == "y"):
+			# Ignore first row, which is attributes name.
+			pass;
+		else:
+			string_for_act_pt += "{x: ";
+			string_for_act_pt += str(feature[0]);
+			string_for_act_pt += ", y: ";
+			string_for_act_pt += str(feature[1]);
+			string_for_act_pt += ", accuracy: ";
+			string_for_act_pt += str(feature[2]);
+			string_for_act_pt += ", total_confidence: ";
+			string_for_act_pt += str(feature[3]);
+			string_for_act_pt += ", osm_flag: ";
+			string_for_act_pt += str(feature[4]);
+			string_for_act_pt += "}, ";
+	f.write(string_for_act_pt);
+
+
+# This list will hold derived Bus Stop locations from OSM data
+data_osm_bus_stations = list()
+
+
+# Open osm_bus_stations.csv file and collect contained data.
+rel_file_path = "data/osm_bus_stations.csv";
+abs_file_path = os.path.join(proj_dir, rel_file_path)
+with open(abs_file_path, 'rb') as new_data_csv:
+	rows = csv.reader(new_data_csv, delimiter = ',')
+	for row in rows:
+		# Append all rows into data_osm_bus_stations list
+		data_osm_bus_stations.append(row);
+
+
+# Create text file containing Bus Stops derived from downloaded OSM data in suitable format for our web-page
+rel_file_path = "data/osm_bus_stations_for_webpage.txt";
+abs_file_path = os.path.join(proj_dir, rel_file_path)
+with open(abs_file_path, 'w') as f:
+	# Making text file for OSM bus stops data.
+	string_for_osm_bus_stop = "";
+	for feature in data_osm_bus_stations:
+		if (feature[0] == "x" and feature[1] == "y"):
+			# Ignore first row, which is attributes name.
+			pass;
+		else:
+			string_for_osm_bus_stop += "{x: ";
+			string_for_osm_bus_stop += str(feature[0]);
+			string_for_osm_bus_stop += ", y: ";
+			string_for_osm_bus_stop += str(feature[1]);
+			string_for_osm_bus_stop += ", name: \"";
+			string_for_osm_bus_stop += str(feature[2]);
+			string_for_osm_bus_stop += "\"}, ";
+	f.write(string_for_osm_bus_stop);
--- web-page/js/osm_bus_pts.js
+++ web-page/js/osm_bus_pts.js
@@ -1,7 +1,6 @@
 // Selected bus stop locations from OSM Dar Es Salam data
 var list_osm = [
-  {x: 39.157875, y: -6.787085, name: "UDA Bus Terminal"},
-  {x: 39.2391183, y: -6.7719216, name: "Kariakoo Bus Stop"},
-  {x: 39.2460801, y: -6.7764802, name: "Buses going south"},
-  {x: 39.2202122, y: -6.7736117, name: "Mnazi Mmoja"},
+	// Copy and Paste data/osm_bus_stations_for_webpage.txt data below this line
+	{x: 39.2743881, y: -6.8273816, name: "UDA Bus Terminal"}, {x: 39.2728579, y: -6.8200158, name: "Kariakoo Bus Stop"}, {x: 39.2737699, y: -6.8240958, name: "Buses going south"}, {x: 39.2749126, y: -6.8136772, name: "Fire Bus Stop"}, {x: 39.2806505, y: -6.8212236, name: "Mnazi Mmoja"}, {x: 39.2805110, y: -6.8214260, name: "Mnazi Mmoja"}, {x: 39.2802214, y: -6.8184645, name: "Baridi"}, {x: 39.2776894, y: -6.8268270, name: " "}, {x: 39.2772709, y: -6.8266672, name: " "}, {x: 39.2822731, y: -6.8163305, name: "kilimanjaro express"}, {x: 39.2721989, y: -6.8190798, name: "kariakoo bus stand"}, {x: 39.2736192, y: -6.8248461, name: "Shule Ya Uhuru"}, {x: 39.2807269, y: -6.8213656, name: "Mnazi Mmoja"},  
+	// Copy and Paste data/osm_bus_stations_for_webpage.txt data below this line
 ];
